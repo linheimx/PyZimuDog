7f7e444749e3c5c197672110235410c6aaf41164
linheimx@gmail.com <linheimx333666>
2017-11-07 22:15:44 +0800

    update models

--- models.py
+++ models.py
@@ -7,22 +7,29 @@ class Where(Enum):
     shooter = 'shooter'
 
 
-class Movie:
+class Base:
+    @property
+    def to_json(self):
+        return self.__dict__
+
+
+class Movie(Base):
     def __init__(self) -> None:
         self.name = ''
         self.avatar_url = ''
-        self.zimus = list()
+        self.detail_url = ''
 
 
-class Zimu:
+class Zimu(Base):
     def __init__(self) -> None:
         self.name = ''
         self.avatar_url = ''
+        self.detail_url = ''
         self.download_url = ''
 
 
-class PageMovie:
-    def __init__(self) -> None:
-        self.movies = list()
-        self.index = 0
-        self.haveNext = False
+class PageMovie(Base):
+    def __init__(self, movies, currentIndex, haveNext) -> None:
+        self.movies = movies
+        self.currentIndex = currentIndex
+        self.haveNext = haveNext
