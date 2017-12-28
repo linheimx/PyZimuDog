cda79763081b956dd96fcd4fbad72ddd437029d9
linheimx@gmail.com <linheimx333666>
2017-12-28 21:46:41 +0800

    update proj

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, request, json, Response
+from sites.zimuku import get_movie_list, get_zimu_list, get_download_url
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 
 
-@app.route('/api/hello')
-def hello():
-    return 'OK'
+@app.route('/api/movielist', methods=['POST'])
+def movielist():
+    html = request.form['html']
+    ret = get_movie_list(html)
+    return Response(ret, mimetype='application/json')
 
+
+@app.route('/api/zimus', methods=['POST'])
+def zimus():
+    html = request.form['html']
+    ret = get_zimu_list(html)
+    return Response(ret, mimetype='application/json')
+
+
+@app.route('/api/zimu_download_url', methods=['POST'])
+def zimu_download_url():
+    html = request.form['html']
+    ret = get_download_url(html)
+    return Response(ret, mimetype='application/json')
+
+
+@app.route('/')
+def zimudog():
+    ret = 'hello'
+    return Response(ret, mimetype='application/json')
+
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
--- sites/zimuku.py
+++ sites/zimuku.py
@@ -1,7 +1,6 @@
 from functools import lru_cache
-
-import time
 from bs4 import BeautifulSoup, Tag
+from flask import json
 from models import Movie, PageMovie, Zimu, Resp
 from utils import print_obj, fetch_text
 
@@ -10,9 +9,7 @@ api_movies = base_url + '/search?q={movie}&ad=1&p={page_index}'
 
 
 @lru_cache(maxsize=2048)
-def get_movie_list(kw_movie, pageIndex=0):
-    url = api_movies.format(movie=kw_movie, page_index=pageIndex)
-    html = fetch_text(url)
+def get_movie_list(html):
     dom = BeautifulSoup(html, 'html.parser')
     try:
         # 1.movie
@@ -26,9 +23,11 @@ def get_movie_list(kw_movie, pageIndex=0):
         div_page = dom.find('div', 'pagination l clearfix')
         index, haveNext = process_page_next(div_page)
         page = PageMovie(movies, index, haveNext)
-        return Resp(page)
+        resp = Resp(page)
     except Exception as e:
-        return Resp(errorMsg=e.__repr__())
+        resp = Resp(errorMsg=e.__repr__())
+    ret = json.dumps(resp, default=lambda obj: obj.to_json, ensure_ascii=False)
+    return ret
 
 
 def process_page_next(div):
@@ -51,7 +50,7 @@ def process_movie_item(div_item: Tag) -> Movie:
     div1 = div_item.find('div', 'litpic hidden-xs hidden-sm')
     a = div1.findChild()
     # detail_url
-    movie.detail_url = base_url + a['href']
+    movie.detail_url = a['href']
     # avatar
     img = a.findChild()
     movie.avatar_url = img['data-original']
@@ -68,8 +67,7 @@ def i_____________________________________________________():
 
 
 @lru_cache(maxsize=2048)
-def get_zimu_list(url):
-    html = fetch_text(url)
+def get_zimu_list(html):
     dom = BeautifulSoup(html, 'html.parser')
     try:
         tbody = dom.select('div > table > tbody')[0]
@@ -78,18 +76,18 @@ def get_zimu_list(url):
         for tr in trs:
             zimu = process_zimu(tr)
             zimus.append(zimu)
-        return Resp(zimus)
+        resp = Resp(zimus)
     except Exception as e:
-        return Resp(errorMsg=e.__repr__())
-
-    print_obj(zimus)
+        resp = Resp(errorMsg=e.__repr__())
+    ret = json.dumps(resp, default=lambda obj: obj.to_json, ensure_ascii=False)
+    return ret
 
 
 def process_zimu(tr) -> Zimu:
     zimu = Zimu()
     a = tr.select('a')[0]
     zimu.name = a['title']
-    zimu.detail_url = base_url + a['href']
+    zimu.detail_url = a['href']
 
     img = tr.select('img')[0]
     zimu.avatar_url = base_url + img['src']
@@ -101,23 +99,24 @@ def ii_____________________________________________________():
 
 
 @lru_cache(maxsize=2048)
-def get_download_url(url):
-    html = fetch_text(url)
+def get_download_url(html):
     dom = BeautifulSoup(html, 'html.parser')
     try:
-        a = dom.select('a[dlurl]')[0]
-        url = base_url + a['dlurl']
-        return Resp(url)
+        a = dom.select('a#down1')[0]
+        url = base_url + a['href']
+        resp = Resp(url)
     except Exception as e:
-        return Resp(errorMsg=e.__repr__())
+        resp = Resp(errorMsg=e.__repr__())
 
+    ret = json.dumps(resp, default=lambda obj: obj.to_json, ensure_ascii=False)
+    return ret
 
-if __name__ == '__main__':
-    # page = get_movie_list('越狱')
-    # print(page.json())
-
-    # ret = get_zimu_list('http://www.zimuku.net/subs/32677.html')
-    # print(ret.json())
-
-    r = get_download_url('http://www.zimuku.net/detail/44173.html')
-    print(r.json())
+# if __name__ == '__main__':
+#     # page = get_movie_list('越狱')
+#     # print(page.json())
+#
+#     # ret = get_zimu_list('http://www.zimuku.net/subs/32677.html')
+#     # print(ret.json())
+#
+#     r = get_download_url('http://www.zimuku.net/detail/44173.html')
+#     print(r.json())
